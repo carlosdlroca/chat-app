bdfc3160d7b16e59ca66d78a3f478c1da2acdb40
Carlos De La Roca <carlosdlroca80@gmail.com>
2020-07-11 10:26:12 -0700

    Isolate server and client apps

--- server/app.js
+++ server/app.js
@@ -1,7 +1,6 @@
 const express = require("express");
 const http = require("http");
 const morgan = require("morgan");
-const path = require("path");
 const app = express();
 const server = http.createServer(app);
 const errorHandler = require("./handlers/error");
@@ -12,9 +11,7 @@ const PORT = process.env.PORT || 3001;
 
 // Initialize and link socket to server
 require("./config/socket.js")(server);
-
 app.use(morgan("dev"));
-app.use(express.static(path.join(__dirname, "client/build")));
 
 app.use("/api/", authRoutes);
 
