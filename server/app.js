31db7efbf4e517295dd222889c12a03e2bed4c94
Carlos De La Roca <carlosdlroca80@gmail.com>
2020-07-11 11:01:43 -0700

    Install body-parser

--- server/app.js
+++ server/app.js
@@ -1,6 +1,8 @@
+require("dotenv").config({ path: "../.env" });
 const express = require("express");
 const http = require("http");
 const morgan = require("morgan");
+const bodyParser = require("body-parser");
 const app = express();
 const server = http.createServer(app);
 const errorHandler = require("./handlers/error");
@@ -12,7 +14,7 @@ const PORT = process.env.PORT || 3001;
 // Initialize and link socket to server
 require("./config/socket.js")(server);
 app.use(morgan("dev"));
-
+app.use(bodyParser.json());
 app.use("/api/", authRoutes);
 
 app.use((req, res, next) => {
