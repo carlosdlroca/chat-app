ef70c2f49cd04dfd38e1be8af51bfb23b5ee6772
Carlos De La Roca <carlosdlroca80@gmail.com>
2020-07-13 19:17:59 -0700

    Edit styles

--- client/src/shared/components/Button/index.js
+++ client/src/shared/components/Button/index.js
@@ -3,23 +3,21 @@ import styled from "styled-components";
 const Button = styled.button`
     border: none;
     border-radius: 1rem;
-    box-shadow: 0 1rem 0.2rem rgba(0, 0, 0, 0.2),
-        0 1rem 0.2rem rgba(0, 0, 0, 0.5);
+    box-shadow: 0 0.5rem 0.2rem rgba(0, 0, 0, 0.2);
     color: white;
     font-size: 2rem;
     padding: 1rem 2rem;
-
+    transition: all 0.2s ease;
     &:hover,
     &:focus {
+        cursor: pointer;
         transform: translateY(-0.4rem);
-        box-shadow: 0 1.4rem 0.2rem rgba(0, 0, 0, 0.3),
-            0 1rem 0.6rem rgba(0, 0, 0, 0.7);
+        box-shadow: 0 0.8rem 0.2rem rgba(0, 0, 0, 0.3);
     }
 
     &:active {
         transform: translateY(-0.2rem);
-        box-shadow: 0 1.2rem 0.2rem rgba(0, 0, 0, 0.3),
-            0 1rem 0.4rem rgba(0, 0, 0, 0.4);
+        box-shadow: 0 0.3rem 0.2rem rgba(0, 0, 0, 0.3);
     }
 `;
 
--- client/src/shared/components/Form/index.js
+++ client/src/shared/components/Form/index.js
@@ -6,20 +6,31 @@ export const Form = styled.form`
     align-items: center;
 `;
 
+export const FormTitle = styled.h1`
+    font-size: 2.7rem;
+    margin-bottom: 1rem;
+`;
+
 export const InputGroup = styled.div`
     display: flex;
     flex-direction: column;
     align-items: flex-start;
+
+    &:not(:last-child) {
+        margin-bottom: 1.5rem;
+    }
 `;
 
 export const InputLabel = styled.label`
-    font-size: 3rem;
+    font-size: 2.2rem;
     text-decoration: underline;
     text-decoration-color: rebeccapurple;
+    margin-bottom: 1rem;
 `;
 
 export const Input = styled.input`
     font-size: 2rem;
+    padding: 0.5rem;
     &::placeholder {
         font-size: 1.8rem;
     }
