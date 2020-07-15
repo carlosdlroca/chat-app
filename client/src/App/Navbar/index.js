421a42e659e034dd0c9a282c7d2e4d4e0b05786e
Carlos De La Roca <carlosdlroca80@gmail.com>
2020-07-14 20:40:25 -0700

    Create logout action creator

--- client/src/App/Navbar/index.js
+++ client/src/App/Navbar/index.js
@@ -1,25 +1,36 @@
 import React, { useContext } from "react";
 import { Link } from "react-router-dom";
-import { StyledNavbar } from "./Styles";
+import { StyledNavbar, NavLinks, NavLink, NavItem } from "./Styles";
 import { StoreContext } from "store/";
-import { setUser } from "store/actions/auth";
+import { logout } from "store/actions/auth";
 
 export default function Navbar() {
     const [{ isAuthenticated, user }, dispatch] = useContext(StoreContext);
     return (
         <StyledNavbar>
-            <Link to='/'>Home</Link>
-            {isAuthenticated ? (
-                <>
-                    <span>Welcome, {user.username}</span>
-                    <span onClick={() => dispatch(setUser({}))}>Logout</span>
-                </>
-            ) : (
-                <>
-                    <Link to='/auth/signin'>Sign in</Link>
-                    <Link to='/auth/signup'>Sign up</Link>
-                </>
-            )}
+            <NavLinks>
+                <Link to='/'>
+                    <NavLink>Home</NavLink>
+                </Link>
+                {isAuthenticated ? (
+                    <>
+                        <NavItem>Welcome, {user.username}</NavItem>
+                        <NavLink onClick={() => dispatch(logout())}>
+                            Logout
+                        </NavLink>
+                    </>
+                ) : (
+                    <>
+                        <Link to='/auth/signin'>
+                            <NavLink>Sign in</NavLink>
+                        </Link>
+
+                        <Link to='/auth/signup'>
+                            <NavLink>Sign up</NavLink>
+                        </Link>
+                    </>
+                )}
+            </NavLinks>
         </StyledNavbar>
     );
 }
