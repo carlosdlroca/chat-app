e9ea865671bc859d427069f7d98d4090e7c60a1a
Carlos De La Roca <carlosdlroca80@gmail.com>
2020-07-14 19:15:32 -0700

    Add Auth to navbar

--- client/src/App/Navbar/index.js
+++ client/src/App/Navbar/index.js
@@ -1,13 +1,25 @@
-import React from "react";
+import React, { useContext } from "react";
 import { Link } from "react-router-dom";
 import { StyledNavbar } from "./Styles";
+import { StoreContext } from "store/";
+import { setUser } from "store/actions/auth";
 
 export default function Navbar() {
+    const [{ isAuthenticated, user }, dispatch] = useContext(StoreContext);
     return (
         <StyledNavbar>
             <Link to='/'>Home</Link>
-            <Link to='/auth/signin'>Sign in</Link>
-            <Link to='/auth/signup'>Sign up</Link>
+            {isAuthenticated ? (
+                <>
+                    <span>Welcome, {user.username}</span>
+                    <span onClick={() => dispatch(setUser({}))}>Logout</span>
+                </>
+            ) : (
+                <>
+                    <Link to='/auth/signin'>Sign in</Link>
+                    <Link to='/auth/signup'>Sign up</Link>
+                </>
+            )}
         </StyledNavbar>
     );
 }
