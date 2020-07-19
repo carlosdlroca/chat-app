16219c9a8fd4427b960bf5dd7fd97248a980e9d6
Carlos De La Roca <carlosdlroca80@gmail.com>
2020-07-18 19:45:02 -0700

    Add default colors

--- client/src/App/Styles/BaseStyles.js
+++ client/src/App/Styles/BaseStyles.js
@@ -1,6 +1,16 @@
 import { createGlobalStyle } from "styled-components";
+import { DEFAULT_COLORS } from "./themes";
 
 export default createGlobalStyle`
+
+    :root {
+        --color-primary: ${DEFAULT_COLORS.primary};
+        --color-secondary: ${DEFAULT_COLORS.secondary};
+        --color-tertiary: ${DEFAULT_COLORS.tertiary};
+        --color-info: ${DEFAULT_COLORS.info};
+        --color-warning: ${DEFAULT_COLORS.warning};
+    }
+
     *, *::after, *::before {
         box-sizing: inherit;
         margin: 0;
---
+++ client/src/App/Styles/themes.js
@@ -0,0 +1,7 @@
+export const DEFAULT_COLORS = {
+    primary: "#66FF99",
+    secondary: "#6666FF",
+    tertiary: "#9966FF",
+    info: "#FF9966",
+    warning: "#FF6666",
+};
