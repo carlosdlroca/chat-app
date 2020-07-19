7ed2731def49d2d55d989d2dc094827337a269af
Carlos De La Roca <carlosdlroca80@gmail.com>
2020-07-19 14:43:52 -0700

    Export Models from index file

--- server/models/index.js
+++ server/models/index.js
@@ -9,3 +9,5 @@ mongoose.connect("mongodb://127.0.0.1:27017", {
 });
 
 module.exports.User = require("./user");
+module.exports.Message = require("./message");
+module.exports.Chatroom = require("./chatroom");
