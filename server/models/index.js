bab181f8d186812a3f5e896ce262645caa2bfef5
Carlos De La Roca <carlosdlroca80@gmail.com>
2020-07-20 10:29:37 -0700

    Remove deprecation warning

--- server/models/index.js
+++ server/models/index.js
@@ -1,6 +1,7 @@
 const mongoose = require("mongoose");
 
 mongoose.set("debug", true);
+mongoose.set("useCreateIndex", true);
 mongoose.Promise = Promise;
 mongoose.connect("mongodb://127.0.0.1:27017", {
     keepAlive: true,
