540f06150cfa0542c5f8d3dc16cd8dc9d18c73a8
Carlos De La Roca <carlosdlroca80@gmail.com>
2020-07-22 17:07:16 -0700

    Add callback to useErrorAlert

--- client/src/shared/hooks/useErrorAlert.js
+++ client/src/shared/hooks/useErrorAlert.js
@@ -1,11 +1,12 @@
 import { useEffect } from "react";
 import { useAlert } from "react-alert";
 
-export default function useErrorAlert(errorObj) {
+export default function useErrorAlert(error, callback) {
     const alert = useAlert();
     useEffect(() => {
         if (error && error.message) {
             alert.error(error.message);
+            callback();
         }
     }, [error, alert]);
 }
