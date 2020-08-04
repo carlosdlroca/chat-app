d75e73b8793c49dfacee119652bba7c107b99d4d
Carlos De La Roca <carlosdlroca80@gmail.com>
2020-08-03 17:15:59 -0700

    Transform AuthPage with withError

--- client/src/Auth/AuthPage.js
+++ client/src/Auth/AuthPage.js
@@ -1,11 +1,14 @@
 import React from "react";
 import AuthForm from "./AuthForm";
 import Page from "shared/components/Page";
+import withError from "shared/hocs/withError";
 
-export default function AuthPage({ authMethod, authTitle }) {
+function AuthPage({ authMethod, authTitle }) {
     return (
         <Page>
             <AuthForm authMethod={authMethod} authTitle={authTitle} />
         </Page>
     );
 }
+
+export default withError(AuthPage);
