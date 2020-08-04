acc30398145d7b958fdefabe1bc5035055f7f8d8
Carlos De La Roca <carlosdlroca80@gmail.com>
2020-08-03 17:11:06 -0700

    Create withError higher-order component

---
+++ client/src/shared/hocs/withError.js
@@ -0,0 +1,24 @@
+import React, { useEffect } from "react";
+import { useAlert } from "react-alert";
+import { connect } from "react-redux";
+import { removeError } from "store/actions/errors";
+
+export default function withError(Component) {
+    function WrappedComponent({ error, removeError, ...props }) {
+        const alert = useAlert();
+        useEffect(() => {
+            if (error && error.message) {
+                alert.error(error.message);
+            }
+            return () => {
+                if (error && error.message) {
+                    removeError();
+                }
+            };
+        }, [error, removeError, error.message, alert]);
+        return <Component {...props} />;
+    }
+
+    const mapStateToProps = ({ error }) => ({ error });
+    return connect(mapStateToProps, { removeError })(WrappedComponent);
+}
